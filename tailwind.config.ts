aff35c6de1ab6e43eaad7b372ec4252741582202
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-03 23:43:07 +0100

    Feat: Added project color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
+  themes: [
+    {
+      mytheme: {
+        primary: "#000503",
+
+        secondary: "#00FF00",
+
+        accent: "#B0ADAD",
+
+        neutral: "#ffffff",
+
+        "base-100": "#ffffff",
+
+        info: "#62CCF9",
+
+        success: "#2CC98F",
+
+        warning: "#FBC337",
+
+        error: "#F76464",
       },
     },
-  },
-  plugins: [],
-}
-export default config
+  ],
+
+  plugins: [require("daisyui")],
+};
+export default config;
