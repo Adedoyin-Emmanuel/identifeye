719f61eb0cb1f79016a35a151561d3c7fafb278a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-04 07:11:53 +0100

    Feat: Finished OTP page

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,19 +1,39 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
+import Text from "@/app/components/Text/Text";
+import Input from "@/app/components/Input/Input";
+import Button from "@/app/components/Button/Button";
+import Link from "next/link";
 
+const ForgotPassword = () => {
+  const pathname = usePathname();
 
-const Index = () => {
-    const pathname = usePathname();
+  return (
+    <section className="w-screen h-screen flex items-center justify-center">
+      <form className="w-11/12 md:w-1/2 xl:w-1/4">
+        <section className="header-section my-8">
+          <h3 className="text-4xl capitalize font-bold text-secondary">
+            Forgot Password
+          </h3>
+          <Text className="py-1">
+            Enter the email associated with your acccount
+          </Text>
+        </section>
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+        <section className="my-4 mb-5">
+          <label htmlFor="email" className="text-md block my-2">
+            Email
+          </label>
+          <Input type="email" name="email" placeholder="Enter your email" />
+        </section>
 
-export default Index;
-    
+        <section className="my-4 mb-5 w-full">
+          <Button>Send Email</Button>
+        </section>
+      </form>
+    </section>
+  );
+};
+
+export default ForgotPassword;
--- app/auth/otp/page.tsx
+++ app/auth/otp/page.tsx
@@ -1,19 +1,61 @@
+"use client";
 
-"use client"
-import React from "react";
+import React, { useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
+import Text from "@/app/components/Text/Text";
+import Input from "@/app/components/Input/Input";
+import Button from "@/app/components/Button/Button";
+import Link from "next/link";
 
+const OTPPage = () => {
+  const pathname = usePathname();
+  const [otp, setOtp] = useState("");
 
-const Index = () => {
-    const pathname = usePathname();
+  const handleOtpChange = (e: React.FormEvent<HTMLInputElement> | any) => {
+    // Handle OTP input change
+    setOtp(e.target.value);
+  };
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+  const handleVerifyOtp = () => {
+    // TODO: Implement OTP verification logic here
+    console.log("Verifying OTP:", otp);
+  };
 
-export default Index;
-    
+  return (
+    <section className="w-screen h-screen flex items-center justify-center">
+      <form className="w-11/12 md:w-1/2 xl:w-1/4">
+        <section className="header-section my-8">
+          <h3 className="text-4xl capitalize font-bold text-secondary">
+            Verify OTP
+          </h3>
+          <Text className="py-1">Enter the OTP sent to your email</Text>
+        </section>
+
+        <section className="my-4 mb-5">
+          <label htmlFor="otp" className="text-md block my-2">
+            OTP
+          </label>
+          <Input
+            type="text"
+            name="otp"
+            value={otp}
+            onChange={handleOtpChange}
+            placeholder="Enter your OTP"
+          />
+        </section>
+
+        <section className="my-4 mb-5 w-full">
+          <Button onClick={handleVerifyOtp}>Verify OTP</Button>
+        </section>
+
+        <section className="my-4 mb-5">
+          <Text className="text-sm">
+            Didnt receive the OTP? <Link href="/resend-otp">Resend OTP</Link>
+          </Text>
+        </section>
+      </form>
+    </section>
+  );
+};
+
+export default OTPPage;
