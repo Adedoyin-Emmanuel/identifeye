27e54f0cb38e1b10fcc269ad468f3a9e937e841e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-04 06:42:16 +0100

    Feat: Finished signup page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -4,6 +4,7 @@ import { useRouter, usePathname } from "next/navigation";
 import Text from "@/app/components/Text/Text";
 import Input from "@/app/components/Input/Input";
 import Button from "@/app/components/Button/Button";
+import Link from "next/link";
 
 const Signup = () => {
   const pathname = usePathname();
@@ -13,7 +14,7 @@ const Signup = () => {
       <form className="w-11/12 md:w-1/2 xl:w-1/4">
         <section className="header-section my-8">
           <h3 className="text-4xl capitalize font-bold text-secondary">
-            Login
+            Signup
           </h3>
           <Text className="py-1">create an account</Text>
         </section>
@@ -36,8 +37,37 @@ const Signup = () => {
           />
         </section>
 
+        <section className="my-4 mb-5">
+          <label htmlFor="password" className="text-md block my-2">
+            Password
+          </label>
+          <Input
+            type="password"
+            name="password"
+            placeholder="shhhhhhhhhh it's a secret"
+          />
+        </section>
+
         <section className="my-4 mb-5 w-full">
           <Button>create account</Button>
+        </section>
+
+        <section>
+          <Text className="inline">
+            no account?
+            <Link
+              className="capitalize text-secondary px-1"
+              href={"/auth/signup"}
+            >
+              create account
+            </Link>
+            <Link
+              className="capitalize text-secondary px-1"
+              href={"/auth/forgot-password"}
+            >
+              Forgot Password
+            </Link>
+          </Text>
         </section>
       </form>
     </section>
