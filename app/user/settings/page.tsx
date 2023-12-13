76699488afeed4719c6a02d89af24cb8a1830987
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-13 08:49:31 +0100

    Feat: Finished settings page

--- app/user/settings/page.tsx
+++ app/user/settings/page.tsx
@@ -12,124 +12,141 @@ const Settings = () => {
   return (
     <div className="w-screen h-screen">
       <SidebarLayout>
-        <section className="header md:my-10 my-5">
-          <h3 className="text-2xl font-bold capitalize">Settings</h3>
-          <Text className="py-1">Setup your preferences</Text>
-        </section>
-
-        <section className="user-preference  md:w-1/2">
-          <section className="light-mode flexjustify-between">
-            <Text className="block">Light mode</Text>
-            <input
-              type="checkbox"
-              className="toggle toggle-md bg-white toggle-secondary"
-              checked
-            />
+        <form>
+          <section className="header md:my-10 my-5">
+            <h3 className="text-2xl font-bold capitalize">Settings</h3>
+            <Text className="py-1">Setup your preferences</Text>
           </section>
 
-          <section className="light-mode flexjustify-between">
-            <Text className="block">Auto download verified credentials</Text>
-            <input
-              type="checkbox"
-              className="toggle toggle-md bg-white toggle-secondary"
-              checked
-            />
-          </section>
+          <section className="user-preference  md:w-1/2 flex flex-col gap-y-12">
+            <section className="light-mode flex justify-between">
+              <Text className="block">Light mode</Text>
+              <input
+                type="checkbox"
+                className="toggle toggle-md bg-white toggle-secondary"
+              />
+            </section>
 
-          <section className="light-mode flexjustify-between">
-            <Text className="block">Start IdentifEye at login</Text>
-            <input
-              type="checkbox"
-              className="toggle toggle-md bg-white toggle-secondary"
-              checked
-            />
-          </section>
+            <section className="light-mode flex justify-between">
+              <Text className="block">Auto download verified credentials</Text>
+              <input
+                type="checkbox"
+                className="toggle toggle-md bg-white toggle-secondary"
+              />
+            </section>
 
-          <section className="light-mode flexjustify-between">
-            <Text className="block">Sync DID with other devices</Text>
-            <input
-              type="checkbox"
-              className="toggle toggle-md bg-white toggle-secondary"
-            />
-          </section>
-        </section>
+            <section className="light-mode flex justify-between">
+              <Text className="block">Start IdentifEye at login</Text>
+              <input
+                type="checkbox"
+                className="toggle toggle-md bg-white toggle-secondary"
+              />
+            </section>
 
-        <hr />
-
-        <section className="general md:w-1/2">
-          <section className="change-avatar flex">
-            <Text>Change Avatar</Text>
-            <section className="avatar bg-accent rounded-full h-10 w-10 font-bold capitalize">
-              TA
+            <section className="light-mode flex justify-between">
+              <Text className="block">Sync DID with other devices</Text>
+              <input
+                type="checkbox"
+                className="toggle toggle-md bg-white toggle-secondary"
+              />
             </section>
           </section>
 
-          <section className="lanaguage flex">
-            <Text>Language</Text>
-            <section className="input">
-              <select
-                className="select border-2 border-gray-300 focus:outline-none rounded-md w-full h-16"
-                name="languages"
+          <br />
+          <section className="border-t border-[#373636]"></section>
+
+          <section className="general md:w-1/2 my-10">
+            <h3 className="text-2xl font-bold capitalize my-2">General</h3>
+            <section className="change-avatar flex items-center justify-between">
+              <Text>Change Avatar</Text>
+              <section className="avatar bg-accent rounded-full h-10 w-10 font-bold capitalize text-primary flex items-center justify-center">
+                TA
+              </section>
+            </section>
+
+            <section className="lanaguage my-2">
+              <Text>Language</Text>
+              <section className="language">
+                <select
+                  className="select border-2 border-gray-300 focus:outline-none rounded-md w-full h-16 "
+                  name="language"
+                >
+                  <option value="System Default">System Default</option>
+                </select>
+              </section>
+            </section>
+
+            <br />
+
+            <section className="2-factor-auth my-2">
+              <Text>2 Factor Authentication</Text>
+              <section className="2-factor-auth">
+                <select
+                  className="select border-2 border-gray-300 focus:outline-none rounded-md w-full h-16 "
+                  name="2-factor-auth"
+                >
+                  <option value="public key">Public Key</option>
+                </select>
+              </section>
+            </section>
+          </section>
+
+          <section className="contact md:w-1/2">
+            <h3 className="text-2xl font-bold capitalize my-1">Contact Us</h3>
+            <Text className="mb-10">
+              Would you like to connect with us through our socials?
+            </Text>
+
+            <section className="socials flex flex-col gap-4">
+              <Link
+                className="capitalize text-secondary px-1"
+                href={"/contact-us"}
               >
-                <option value="system default">System Default</option>
-              </select>
-            </section>
-          </section>
-
-          <section className="2-factor-auth flex">
-            <Text>2 Factor Authentication</Text>
-            <section className="2-factor-auth">
-              <select
-                className="select border-2 border-gray-300 focus:outline-none rounded-md w-full h-16"
-                name="2-factor-auth"
+                Contact US
+              </Link>
+              <Link
+                className="capitalize text-secondary px-1"
+                href={"/help-center"}
               >
-                <option value="public key">Public Key</option>
-              </select>
+                Help Center
+              </Link>
+              <Link
+                className="capitalize text-secondary px-1"
+                href={"/license"}
+              >
+                Licenses
+              </Link>
+
+              <Link
+                className="capitalize text-secondary px-1"
+                href={"/privacy-policy"}
+              >
+                Terms And Privacy Policy
+              </Link>
+
+              <Link
+                className="capitalize text-secondary px-1"
+                href={"/about-us"}
+              >
+                About Us
+              </Link>
             </section>
           </section>
-        </section>
 
-        <section className="contact md:w-1/2">
-          <Text>Would you like to connect with us through our socials?</Text>
+          <br />
+          <br />
 
-          <section className="socials">
-            <Link
-              className="capitalize text-secondary px-1"
-              href={"/contact-us"}
-            >
-              Contact US
-            </Link>
-            <Link
-              className="capitalize text-secondary px-1"
-              href={"/help-center"}
-            >
-              Help Center
-            </Link>
-            <Link className="capitalize text-secondary px-1" href={"/license"}>
-              Licenses
-            </Link>
+          <section className="danger md:w-1/2">
+            <section className="w-full">
+              <Button className="w-44 hover:bg-error">Delete Account</Button>
+            </section>
 
-            <Link
-              className="capitalize text-secondary px-1"
-              href={"/privacy-policy"}
-            >
-              Terms And Privacy Policy
-            </Link>
-
-            <Link className="capitalize text-secondary px-1" href={"/about-us"}>
-              About Us
-            </Link>
+            <Text className="text-error text-sm my-5">
+              Warning! this is a destructive action. Your data stored on our
+              Decentralized Web Nodes would be deleted alongside{" "}
+            </Text>
           </section>
-        </section>
-
-        <section className="danger md:w-1/2">
-          <Text className="text-danger">
-            Warning! this is a destructive action. Your data stored on our
-            Decentralized Web Nodes would be deleted alongside{" "}
-          </Text>
-
-          <Button>Delete Account</Button>
-        </section>
+        </form>
       </SidebarLayout>
     </div>
   );
