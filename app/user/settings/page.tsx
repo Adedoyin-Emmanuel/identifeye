1f514c91c05e2b35aac614997fc698f6584f1a54
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-13 08:20:11 +0100

    Feat: Added toggle button

--- app/user/settings/page.tsx
+++ app/user/settings/page.tsx
@@ -1,19 +1,34 @@
-
-"use client"
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import { useRouter, usePathname } from "next/navigation";
+import SidebarLayout from "@/app/components/SidebarLayout/SidebarLayout";
+import Text from "@/app/components/Text/Text";
+import Button from "@/app/components/Button/Button";
+import Input from "@/app/components/Input/Input";
 
+const Settings = () => {
+  const pathname = usePathname();
 
-const Index = () => {
-    const pathname = usePathname();
+  return (
+    <div className="w-screen h-screen">
+      <SidebarLayout>
+        <section className="header md:my-10 my-5">
+          <h3 className="text-2xl font-bold capitalize">Settings</h3>
+          <Text className="py-1">Setup your preferences</Text>
+        </section>
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+        <section className="user-preference">
+          <section className="light-mode flex w-1/2 justify-between">
+            <Text className="block">Light mode</Text>
+            <input
+              type="checkbox"
+              className="toggle toggle-md bg-white toggle-secondary"
+            />
+          </section>
+        </section>
+      </SidebarLayout>
+    </div>
+  );
+};
 
-export default Index;
-    
+export default Settings;
