33d6c4bc61d455148d62f2da354855feab51d1a0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-13 04:57:44 +0100

    Feat: Added header component

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+interface HeaderProps {}
+
+const Header = () => {
+  return (
+    <nav className="w-screen flex items-center md:justify-center justify-between md:flex-row flex-col  py-5 overflow-x-hidden z-10">
+      <h1>Index works!</h1>
+    </nav>
+  );
+};
+
+export default Header;
